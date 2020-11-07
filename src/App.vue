506164c7b20665df2c5438b358edefbc80331b9a
Agustina <maria.agustina.valenti89@gmail.com>
2020-11-07 12:37:18 -0300

    layout

---
+++ src/App.vue
@@ -0,0 +1,96 @@
+<template>
+	<v-app>
+		<v-navigation-drawer app v-model="drawer" absolute bottom temporary>
+			<v-list nav dense>
+				<v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
+					<v-list-item>
+						<v-list-item-title>Foo</v-list-item-title>
+					</v-list-item>
+
+					<v-list-item>
+						<v-list-item-title>Bar</v-list-item-title>
+					</v-list-item>
+
+					<v-list-item>
+						<v-list-item-title>Fizz</v-list-item-title>
+					</v-list-item>
+
+					<v-list-item>
+						<v-list-item-title>Buzz</v-list-item-title>
+					</v-list-item>
+				</v-list-item-group>
+			</v-list>
+		</v-navigation-drawer>
+
+		<v-app-bar app color="white" class="teal--text text--lighten-1 mx-4 teal--text"  elevate-on-scroll>
+			<v-app-bar-nav-icon class="mx-4 teal--text"  @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+			<v-toolbar-title>My files</v-toolbar-title>
+
+			<v-spacer></v-spacer>
+
+	
+
+			<v-btn icon>
+				<v-icon></v-icon>
+			</v-btn>
+
+		</v-app-bar>
+
+		<!-- Sizes your content based upon application components -->
+		<v-main>
+			<!-- Provides the application the proper gutter -->
+			<v-container fluid>
+				<router-view></router-view>
+			</v-container>
+		</v-main>
+
+		<v-footer app color="white teal--text" >
+			<v-card flat tile class="white teal--text text--lighten-1 text-center">
+				<v-card-text>
+					<v-btn v-for="icon in icons" :key="icon" class="mx-4 teal--text" icon>
+						<v-icon size="24px">
+							{{ icon }}
+						</v-icon>
+					</v-btn>
+				</v-card-text>
+
+				<v-card-text class="mx-4 teal--text pt-0">
+					Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum.
+					Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur
+					dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada.
+					Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel
+					diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes,
+					nascetur ridiculus mus.
+				</v-card-text>
+
+				<v-divider></v-divider>
+
+				<v-card-text class="mx-4 teal--text">
+					{{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+				</v-card-text>
+			</v-card>
+		</v-footer>
+	</v-app>
+</template>
+
+<script>
+export default {
+	data: () => ({
+		drawer: false,
+		group: null,
+		icons: ['mdi-facebook', 'mdi-twitter', 'mdi-linkedin', 'mdi-instagram'],
+	}),
+
+	watch: {
+		group() {
+			this.drawer = false;
+		},
+	},
+};
+</script>
+
+
+<style >
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+	<div>
+		<v-parallax
+			dark
+			height="1000"
+			src="https://images2.minutemediacdn.com/image/upload/c_fill,g_auto,h_1248,w_2220/v1555352156/shape/mentalfloss/istock_000056850756_small.jpg?itok=1lz6dqnS"
+		>
+			<v-row align="center" justify="center">
+				<v-col class="text-center" cols="12">
+					<h1 class="display-1 font-weight-thin mb-4">
+						Turismo Buenos Aires
+					</h1>
+					<h4 class="subheading">
+						Todo lo que necesitas saber
+					</h4>
+				</v-col>
+			</v-row>
+		</v-parallax>
+
+		<v-main>
+			<!-- Provides the application the proper gutter -->
+			<v-container fluid>
+				<GridUno />
+        	<Experiencias />
+				<router-view></router-view>
+			</v-container>
+		</v-main>
+	</div>
+</template>
+
+<script>
+import GridUno from '@/components/GridUno.vue';
+import Experiencias from '@/components/Experiencias.vue';
+
+
+export default {
+	name: 'Home',
+	components: {
+    GridUno,
+    Experiencias
+	},
+	data: () => ({
+		alignments: ['start', 'center', 'end'],
+	}),
+};
+</script>
