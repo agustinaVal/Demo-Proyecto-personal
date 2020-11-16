75644c1587eeb27c125d231721f88b2586ef9ff8
Agustina <maria.agustina.valenti89@gmail.com>
2020-11-16 17:42:18 -0300

    arreglando bugs de horario en registro

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -5,35 +5,32 @@
 		<v-text-field v-model="excursionData.description" label="Descripcion" required></v-text-field>
 		<v-text-field v-model="excursionData.price" label="Precios" required></v-text-field>
 		<v-text-field v-model="excursionData.stock" label="Stock" required></v-text-field>
+		<v-text-field  
+         v-model="excursionData.time"   label="Horario" required></v-text-field>
+<v-text-field
+      v-for="(horario, i) in excursion.time"
+      :key="i"
+      v-model="excursion.time[i]"
+      label="Horario"
+      required
+    ></v-text-field>
 
-		<h3>Horarios:</h3>
-		<v-text-field
-			v-for="(excursion, i) in excursionData.time"
-			:key="i"
-			v-model="excursionData.time[i]"
-			label="Hora"
-			required
-		></v-text-field>
-		<v-btn class="mx-2" fab small dark color="success" @click="add">
-			<v-icon dark>
-				mdi-plus
-			</v-icon>
-		</v-btn>
+<v-btn class="mx-2" fab small dark color="success" @click="add">
+      <v-icon dark>
+        mdi-plus
+      </v-icon>
+    </v-btn>
 
-		<v-btn class="mx-2" fab small dark color="red" @click="Delete">
-			<v-icon dark>
-				mdi-delete
-			</v-icon>
-		</v-btn>
+    <v-btn class="mx-2" fab small dark color="red" @click="Delete">
+      <v-icon dark>
+        mdi-delete
+      </v-icon>
+    </v-btn>
 
-		<br />
-		<br />
-		<v-btn>
-			Agregar actividad
-		</v-btn>
-		<v-btn>
-			editar pizza
-		</v-btn>
+    <br />
+    <br />
+
+		
 	</v-form>
 </template>
 
@@ -41,17 +38,28 @@
 import { mapActions, mapGetters } from 'vuex';
 export default {
 	name: 'Form',
+	data: () => ({
+		excursion:{
+            title : "",
+            img : "",
+            description : "",
+            price : "",
+            stock : "",
+            time : [],
+
+        }
+	}),
 
 	computed: {
 		...mapGetters(['excursionData']),
-    },
-    methods:{
-        add() {
-      this.excursionData.time.push("");
+	},
+	methods: {
+	 add() {
+      this.excursion.time.push("");
     },
     Delete() {
-      this.excursionData.time.pop();
+      this.excursion.time.pop();
     },
-    }
+	},
 };
 </script>
