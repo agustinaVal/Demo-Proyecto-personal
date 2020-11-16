d7583f06a23de67be193ef26221686e6b64cdd40
Agustina <maria.agustina.valenti89@gmail.com>
2020-11-16 09:44:16 -0300

    subiendo data a registro

--- src/store/index.js
+++ src/store/index.js
@@ -1,32 +1,44 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import firebase from 'firebase'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import firebase from 'firebase';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    excursiones : [],
-  },
-  mutations: {
-    GET_DATA(state, excursiones) {
-      state.excursiones = excursiones;
-    },
-  },
-  actions: {
-    getData({ commit }) {
-      firebase
-        .firestore()
-        .collection("excurciones")
-        .onSnapshot((snapshot) => {
-          let excursiones = [];
-          snapshot.forEach((p) => {
-            excursiones.push({ id: p.id, data: p.data() });
-          });
-          commit("GET_DATA", excursiones);
-        });
-    },
-  },
-  modules: {
-  }
-})
+	state: {
+		excursiones: [],
+	},
+	mutations: {
+		GET_DATA(state, excursiones) {
+			state.excursiones = excursiones;
+		},
+	},
+	actions: {
+		getData({ commit }) {
+			firebase
+				.firestore()
+				.collection('excurciones')
+				.onSnapshot((snapshot) => {
+					let excursiones = [];
+					snapshot.forEach((p) => {
+						excursiones.push({
+							id: p.id,
+							data: p.data(),
+						});
+					});
+					commit('GET_DATA', excursiones);
+				});
+		},
+	},
+	getters: {
+		excursionData: (state) => {
+			return state.excursiones.map((p) => {
+				const data = p.data;
+				data.id = p.id;
+				return data;
+			});
+		},
+	},
+
+	modules: {},
+});
--- src/views/Registro.vue
+++ src/views/Registro.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<template>
-			<v-data-table :headers="headers" :items="data" class="elevation-1">
+			<v-data-table :headers="headers" :items="excursionData" class="elevation-1">
 				<template v-slot:item.calories="{ item }">
 					<v-chip :color="getColor(item.calories)" dark>
 						{{ item.calories }}
@@ -12,38 +12,36 @@
 	</div>
 </template>
 
-
-
 <script>
-import { mapSate, mapState } from 'vuex'
-  export default {
-      name: 'Registro',
-    data () {
-      return {
-        headers: [
-           {
-          text: "Nombre",
-          align: "start",
-          value: "name",
-        },
-          { text: "Imagen", value: "img" },
-          { text: "Nombre", value: "title" },
-          { text: "Precios", value: "price" },
-          { text: "Stock", value: "stock" },
-          { text: "Horario", value: "time" },
-         { text: "Acciones", value: "acciones" },
-        ],
-      }
-    },
-    methods: {
-      getColor (calories) {
-        if (calories > 400) return 'red'
-        else if (calories > 200) return 'orange'
-        else return 'green'
-      },
-    },
-    computed:{
-        ...mapState(['excursiones'])
-    }
-  }
+import { mapGetters } from 'vuex';
+export default {
+	name: 'Registro',
+	data() {
+		return {
+			headers: [
+				{
+					text: 'Nombre',
+					align: 'start',
+					value: 'name',
+				},
+				{ text: 'Imagen', value: 'img' },
+				{ text: 'Nombre', value: 'title' },
+				{ text: 'Precios', value: 'price' },
+				{ text: 'Stock', value: 'stock' },
+				{ text: 'Horario', value: 'time' },
+				{ text: 'Acciones', value: 'acciones' },
+			],
+		};
+	},
+	methods: {
+		getColor(calories) {
+			if (calories > 400) return 'red';
+			else if (calories > 200) return 'orange';
+			else return 'green';
+		},
+	},
+	computed: {
+		...mapGetters(['excursionData']),
+	},
+};
 </script>
