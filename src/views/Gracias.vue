ed3caef05fd42f9f3f178285d311e6649fb23317
Agustina <maria.agustina.valenti89@gmail.com>
2020-11-24 10:28:42 -0300

    subiendo vista gracias con template

--- src/views/Gracias.vue
+++ src/views/Gracias.vue
@@ -1,13 +1,20 @@
 <template>
-	<h1>hola</h1>
+	<v-container>
+		<h1>¡Gracias por confiar en TusimoBaires!</h1>
+		<h2>Un agente se comunicará contigo</h2>
+
+		<v-img
+			contain
+			max-height="500"
+			max-width="500"
+			src="https://st2.depositphotos.com/2343527/7218/v/450/depositphotos_72181019-stock-illustration-young-man-tourist-with-a.jpg"
+		>
+		</v-img>
+	</v-container>
 </template>
 
 <script>
-
 export default {
 	name: 'Gracias',
-
-	
-}
-
+};
 </script>
