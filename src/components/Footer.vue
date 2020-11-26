fa41e4b8ad0c09f393e62807ffe457a4d63ef47a
Agustina <maria.agustina.valenti89@gmail.com>
2020-11-26 12:17:52 -0300

    home principal stylo terminado

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,36 +1,35 @@
 <template>
-	<div>
-		<v-footer color="white">
-			<v-card flat tile class="white pink--text text--lighten-1 text-center">
-				<v-card-text>
-					<v-btn v-for="icon in icons" :key="icon" class="pink--text" icon>
-						<v-icon size="24px">
-							{{ icon }}
-						</v-icon>
-					</v-btn>
-				</v-card-text>
+  <div>
+    <v-footer class="white pa-16">
+      <v-card flat tile class="white text-center">
+        <v-card-text class="mx-4 footer__text">
+          Disfruta del esparcimiento, la cultura y diversión junto a
+          TurismoBaires. Te brindamos un servicio único en el cual podrás
+          recorrer todos los atractivos de Buenos Airesy sus alrededores.
+        </v-card-text>
 
-				<v-card-text class="mx-4  pink--text pt-0">
-					Disfruta del esparcimiento, la cultura y diversión junto a TurismoBaires.
-					 Te brindamos un servicio único en el cual podrás recorrer todos los atractivos de Buenos Airesy sus alrededores.
-				</v-card-text>
+        <v-divider></v-divider>
 
-				<v-divider></v-divider>
-
-				<v-card-text class="mx-4 pink--text">
-					{{ new Date().getFullYear() }} — <strong>TurismoBaires</strong>
-				</v-card-text>
-			</v-card>
-		</v-footer>
-	</div>
+        <v-card-text class="mx-4" id="footer_final">
+          {{ new Date().getFullYear() }} — <strong>TurismoBaires</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+  </div>
 </template>
 <script>
 export default {
-	name: 'Footer',
-	data: () => ({
-		icons: ['mdi-facebook', 'mdi-twitter', 'mdi-linkedin', 'mdi-instagram'],
-	}),
+  name: "Footer",
+  data: () => ({}),
 };
 </script>
 
-<style></style>
+<style>
+.footer__text {
+  color: #9e9e9e;
+}
+
+#footer_final {
+  color: #00e5ff;
+}
+</style>
