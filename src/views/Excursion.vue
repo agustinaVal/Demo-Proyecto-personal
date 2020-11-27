e568a7fdae27f3c7c91a5ae11fa901bfd909aded
Agustina <maria.agustina.valenti89@gmail.com>
2020-11-27 08:25:12 -0300

    ruta dinamica estilos

--- src/views/Excursion.vue
+++ src/views/Excursion.vue
@@ -1,22 +1,65 @@
+<!-- @format -->
+
 <template>
-    <div>
-        <h1>{{excursion.title}}</h1>
-        <v-btn :to=" {name: 'Home'}">Volver al Inicio</v-btn>
-    </div>
+	<div id="Home">
+		<section id="Her__section">
+			<div class="Home_principal">
+				<v-row align="center" justify="center">
+					<v-col class="text-center" cols="12">
+						<h1 class="display-3 my-5">{{ excursion.title }}</h1>
+						<span class="Home__sub-titulo_principal py-5">{{
+							excursion.subtitle
+						}}</span>
+					</v-col>
+				</v-row>
+				<v-parallax
+					height="650"
+					class="Home__banner_principal"
+					:src="excursion.img"
+				>
+					<v-row align="center" justify="center">
+						<v-col class="text-center" cols="12">
+							<h1 class="display-1 my-5">TurismoBaires</h1>
+						</v-col>
+					</v-row>
+				</v-parallax>
+			</div>
+		</section>
+
+		<section id="Main__que_ver">
+			<div>
+				<v-row align="center" justify="center">
+					<v-col class="text-center" cols="12">
+						<h2 class="Titulo_main_QUEVER">Recorrido</h2>
+						<span class="Home__sub-titulo_principal py-5">{{
+							excursion.description
+						}}</span>
+					</v-col>
+				</v-row>
+
+				<v-row align="center" justify="center">
+					<v-col sm="12" md="4">
+						<v-btn color="red accent-4 my-4 white--text" :to="{ name: 'Home' }"
+							>Volver al Inicio</v-btn
+						>
+					</v-col>
+				</v-row>
+			</div>
+		</section>
+	</div>
 </template>
 
-
 <script>
-import { mapGetters } from 'vuex';
-export default {
-    name: "Excursion",
-    props: ["id"],
-    computed:{
-        ...mapGetters(['getExcursion']),
-        excursion(){
-            const id = this.id
-            return this.getExcursion(id).data;
-        }
-    }
-}
+	import { mapGetters } from 'vuex'
+	export default {
+		name: 'Excursion',
+		props: ['id'],
+		computed: {
+			...mapGetters(['getExcursion']),
+			excursion() {
+				const id = this.id
+				return this.getExcursion(id).data
+			},
+		},
+	}
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,137 +1,140 @@
+<!-- @format -->
+
 <template>
-  <div id="Home">
-    <section id="Her__section">
-      <div class="Home_principal">
-        <v-row align="center" justify="center">
-          <v-col class="text-center" cols="12">
-            <h1 class="Home__titulo_principal">Imperdibles</h1>
-            <span class="Home__sub-titulo_principal py-5"
-              >Si disponés de poco tiempo para visitar la ciudad, te proponemos
-              algunos atractivos que no podés perderte. <br />
-              ¡Disfrutá de una experiencia completa!</span
-            >
-          </v-col>
-        </v-row>
-        <v-parallax
-          height="650"
-          class="Home__banner_principal"
-          src="https://turismo.buenosaires.gob.ar/sites/turismo/files/caminito_la_boca_1200_gente_2.jpg"
-        >
-          <v-row align="center" justify="center">
-            <v-col class="text-center" cols="12">
-              <h3>TurismoBaires</h3>
-            </v-col>
-          </v-row>
-        </v-parallax>
-      </div>
-    </section>
+	<div id="Home">
+		<section id="Her__section">
+			<div class="Home_principal">
+				<v-row align="center" justify="center">
+					<v-col class="text-center" cols="12">
+						<h1 class="Home__titulo_principal">Imperdibles</h1>
+						<span class="Home__sub-titulo_principal py-5"
+							>Si disponés de poco tiempo para visitar la ciudad, te proponemos
+							algunos atractivos que no podés perderte. <br />
+							¡Disfrutá de una experiencia completa!</span
+						>
+					</v-col>
+				</v-row>
+				<v-parallax
+					height="650"
+					class="Home__banner_principal"
+					src="https://turismo.buenosaires.gob.ar/sites/turismo/files/caminito_la_boca_1200_gente_2.jpg"
+				>
+					<v-row align="center" justify="center">
+						<v-col class="text-center" cols="12">
+							<h3 class="display-1 my-10">TurismoBaires</h3>
+						</v-col>
+					</v-row>
+				</v-parallax>
+			</div>
+		</section>
 
-    <section id="Main__que_ver">
-      <div>
-        <v-row align="center" justify="center">
-          <v-col class="text-center" cols="12">
-            <h2 class="Titulo_main_QUEVER">Destacados de la cuidad</h2>
-            <span class="Home__sub-titulo_principal py-5"
-              >Descubre los secretos y la historia de la ciudad a través de sus
-              mejores paseos</span
-            >
-          </v-col>
-        </v-row>
+		<section id="Main__que_ver">
+			<div>
+				<v-row align="center" justify="center">
+					<v-col class="text-center" cols="12">
+						<h2 class="Titulo_main_QUEVER">Destacados de la cuidad</h2>
+						<span class="Home__sub-titulo_principal py-5"
+							>Descubre los secretos y la historia de la ciudad a través de sus
+							mejores paseos</span
+						>
+					</v-col>
+				</v-row>
 
-        <v-row>
-          <v-col sm="12" md="4" v-for="(excursion, i) in excursiones" :key="i"
-            ><Cards :excursion="excursion"
-          /></v-col>
-        </v-row>
-      </div>
-    </section>
+				<v-row>
+					<v-col sm="12" md="4" v-for="(excursion, i) in excursiones" :key="i"
+						><Cards :excursion="excursion"
+					/></v-col>
+				</v-row>
+			</div>
+		</section>
 
-    <section id="Main__donde_dormir">
-      <div>
-        <v-row align="center" justify="center">
-          <v-col class="text-center" col="12" >
-            <h2 class="Titulo_main_QUEVER">
-              Los mejores Hoteles para descansar
-            </h2>
-            <span class="Home__sub-titulo_principal py-5"
-              >Conoce los hoteles mas valorados por los viajeros</span
-            >
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col
-            sm="12" md="4"
-            v-for="(alojamiento, i) in alojamientos"
-            :key="i"
-            ><Experiencias :alojamiento="alojamiento"
-          /></v-col>
-        </v-row>
-      </div>
-    </section>
+		<section id="Main__donde_dormir">
+			<div>
+				<v-row align="center" justify="center">
+					<v-col class="text-center" col="12">
+						<h2 class="Titulo_main_QUEVER">
+							Los mejores Hoteles para descansar
+						</h2>
+						<span class="Home__sub-titulo_principal py-5"
+							>Conoce los hoteles mas valorados por los viajeros</span
+						>
+					</v-col>
+				</v-row>
+				<v-row>
+					<v-col
+						sm="12"
+						md="4"
+						v-for="(alojamiento, i) in alojamientos"
+						:key="i"
+						><Experiencias :alojamiento="alojamiento"
+					/></v-col>
+				</v-row>
+			</div>
+		</section>
 
-    <section id="Main__Galerias de imagenes">
-      <div>
-        <v-row align="center" justify="center">
-          <v-col class="text-center" cols="12">
-            <h2 class="Titulo_main_QUEVER">¡Siente Buenos Aires!</h2>
-            <span class="Home__sub-titulo_principal py-5"
-              >Enamórate de las imágenes</span
-            >
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col> <Galeria /> </v-col>
-        </v-row>
-      </div>
-    </section>
+		<section id="Main__Galerias de imagenes">
+			<div>
+				<v-row align="center" justify="center">
+					<v-col class="text-center" cols="12">
+						<h2 class="Titulo_main_QUEVER">¡Siente Buenos Aires!</h2>
+						<span class="Home__sub-titulo_principal py-5"
+							>Enamórate de las imágenes</span
+						>
+					</v-col>
+				</v-row>
+				<v-row>
+					<v-col> <Galeria /> </v-col>
+				</v-row>
+			</div>
+		</section>
 
-    <section id="Footer">
-      <div>
-        <v-row align="center" justify="center">
-          <v-col class="text-center" sm="12">
-            <Footer />
-          </v-col>
-        </v-row>
-      </div>
-    </section>
-  </div>
+		<section id="Footer">
+			<div>
+				<v-row align="center" justify="center">
+					<v-col class="text-center" sm="12">
+						<Footer />
+					</v-col>
+				</v-row>
+			</div>
+		</section>
+	</div>
 </template>
 
 <script>
-import Cards from "@/components/Cards.vue";
-import Experiencias from "@/components/Experiencias.vue";
-import Galeria from "@/components/Galeria.vue";
-import Footer from "@/components/Footer.vue";
-import { mapState } from "vuex";
-export default {
-  name: "Home",
-  components: {
-    Cards,
-    Experiencias,
-    Galeria,
-    Footer,
-  },
-  data: () => ({}),
-  computed: {
-    ...mapState(["excursiones", "alojamientos"]),
-  },
-  props: {},
-};
+	import Cards from '@/components/Cards.vue'
+	import Experiencias from '@/components/Experiencias.vue'
+	import Galeria from '@/components/Galeria.vue'
+	import Footer from '@/components/Footer.vue'
+	import { mapState } from 'vuex'
+	export default {
+		name: 'Home',
+		components: {
+			Cards,
+			Experiencias,
+			Galeria,
+			Footer,
+		},
+		data: () => ({}),
+		computed: {
+			...mapState(['excursiones', 'alojamientos']),
+		},
+		props: {},
+	}
 </script>
 
 <style>
-.Home__titulo_principal {
-  font-weight: 700;
-  font-size: 60px;
-  color: #d500f9;
-}
-.Home__sub-titulo_principal {
-  color: #9e9e9e;
-  font-size: 18px;
-}
-.Titulo_main_QUEVER {
-  font-weight: 700;
-  font-size: 40px;
-  color: #d500f9;
-}
+	.Home__titulo_principal {
+		font-weight: 700;
+		font-size: 60px;
+		color: #d500f9;
+	}
+	.Home__sub-titulo_principal {
+		color: #9e9e9e;
+		font-size: 18px;
+	}
+	.Titulo_main_QUEVER {
+		font-weight: 700;
+		font-size: 40px;
+		color: #d500f9;
+	}
 </style>
