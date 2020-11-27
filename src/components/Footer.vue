98b79b0eeaf3478ce26b3312306f8d7a160140cc
Agustina <maria.agustina.valenti89@gmail.com>
2020-11-27 17:09:17 -0300

    detalles completo

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,35 +1,37 @@
+<!-- @format -->
+
 <template>
-  <div>
-    <v-footer class="white pa-16">
-      <v-card flat tile class="white text-center">
-        <v-card-text class="mx-4 footer__text">
-          Disfruta del esparcimiento, la cultura y diversión junto a
-          TurismoBaires. Te brindamos un servicio único en el cual podrás
-          recorrer todos los atractivos de Buenos Airesy sus alrededores.
-        </v-card-text>
+	<div>
+		<v-footer class="white pa-16">
+			<v-card flat tile class="white text-center">
+				<v-card-text class="mx-4 footer__text">
+					Disfruta del esparcimiento, la cultura y diversión junto a
+					TurismoBaires. Te brindamos un servicio único en el cual podrás
+					recorrer todos los atractivos de Buenos Aires y sus alrededores.
+				</v-card-text>
 
-        <v-divider></v-divider>
+				<v-divider></v-divider>
 
-        <v-card-text class="mx-4" id="footer_final">
-          {{ new Date().getFullYear() }} — <strong>TurismoBaires</strong>
-        </v-card-text>
-      </v-card>
-    </v-footer>
-  </div>
+				<v-card-text class="mx-4" id="footer_final">
+					{{ new Date().getFullYear() }} — <strong>TurismoBaires</strong>
+				</v-card-text>
+			</v-card>
+		</v-footer>
+	</div>
 </template>
 <script>
-export default {
-  name: "Footer",
-  data: () => ({}),
-};
+	export default {
+		name: 'Footer',
+		data: () => ({}),
+	}
 </script>
 
 <style>
-.footer__text {
-  color: #9e9e9e;
-}
+	.footer__text {
+		color: #9e9e9e;
+	}
 
-#footer_final {
-  color: #00e5ff;
-}
+	#footer_final {
+		color: #00e5ff;
+	}
 </style>
--- src/views/Excursion.vue
+++ src/views/Excursion.vue
@@ -12,49 +12,94 @@
 						}}</span>
 					</v-col>
 				</v-row>
-				<v-img height="650" class="Home__banner_principal" :src="excursion.img">
+				<v-img
+					height="650"
+					class="Home__banner_principal"
+					:src="excursion.imagen"
+				>
 				</v-img>
 			</div>
 		</section>
 
-		<section id="">
+		<section id="Detalle__queVer">
 			<div>
 				<v-row align="center" justify="center">
 					<v-col class="text-center" cols="12">
-						<h2 class="">¿Que esperar?</h2>
-						<span class="py-5">{{ excursion.description }}</span>
+						<h2 class="titulos_detalle_excursion py-2">¿Que esperar?</h2>
+						<p class="py-3">{{ excursion.description }}</p>
 					</v-col>
 				</v-row>
 			</div>
 		</section>
 
-		<section id="">
+		<section id="Detalle__itinerario">
 			<div>
 				<v-row align="center" justify="center">
 					<v-col class="text-center" cols="12">
-						<h2 class="">Recogida</h2>
-						<span class=" py-5">{{ excursion.description }}</span>
+						<h2 class="titulos_detalle_excursion py-2">Itinerario</h2>
+						<p class="py-3">{{ excursion.itinerario }}</p>
+						<span class="py-3">
+							<strong>Duración total</strong> - {{ excursion.duracion }}</span
+						>
 					</v-col>
 				</v-row>
 			</div>
 		</section>
 
-		<section id="">
+		<section id="Detalle__curiosidades">
 			<div>
 				<v-row align="center" justify="center">
 					<v-col class="text-center" cols="12">
-						<h2 class="">Medidas Covid-19</h2>
-						<span class=" py-5">{{ excursion.description }}</span>
+						<h2 class="titulos_detalle_excursion py-2">Curiosidades</h2>
+						<p class=" py-3">{{ excursion.curiosidades }}</p>
 					</v-col>
 				</v-row>
 			</div>
 		</section>
-		<section id="">
+		<section id="Detalle__medidasCovid">
 			<div>
 				<v-row align="center" justify="center">
 					<v-col class="text-center" cols="12">
-						<h2 class="">Detalles</h2>
-						<span class=" py-5">{{ excursion.description }}</span>
+						<h2 class="titulos_detalle_excursion py-2">Medidas Covid-19</h2>
+						<p>
+							Todos nuestros servicios cumplen la legislación local y las
+							recomendaciones sanitarias en materia de protección y seguridad
+							para evitar el contagio del virus Covid-19. En concreto, esta
+							actividad tiene las siguientes medidas:
+						</p>
+						<div class="py-3">
+							<span>
+								<strong>1</strong> - Se mantendrá la distancia de seguridad de 1
+								metro entre clientes siempre que sea posible.
+							</span>
+							<span>
+								<strong>2</strong> - El uso de mascarilla es obligatorio. El
+								cliente debe llevar su propia mascarilla.
+							</span>
+							<span>
+								<strong>3</strong> - Se facilitará gel hidroalcohólico antes,
+								durante y después del servicio.
+							</span>
+							<span>
+								<strong>4</strong> - Todo el personal implicado (guía, chófer,
+								etc..) llevará mascarilla durante la realización del servicio.
+							</span>
+							<span>
+								<strong>5</strong> - Se controlará diariamente la temperatura y
+								síntomas de todo el personal implicado en el servicio excluyendo
+								y manteniendo en cuarentena de 14 días a aquellos que presenten
+								fiebre o síntomas.
+							</span>
+							<span>
+								<strong>6</strong> - Todos los medios de transporte implicados
+								serán desinfectados antes y después de cada servicio y lavados
+								en profundidad cada 24 horas.
+							</span>
+							<span>
+								<strong>7</strong> - Será suficiente con tener el bono o reserva
+								en el móvil y no imprimirlo.
+							</span>
+						</div>
 					</v-col>
 				</v-row>
 			</div>
@@ -87,3 +132,11 @@
 		},
 	}
 </script>
+
+<style>
+	.titulos_detalle_excursion {
+		font-weight: 600;
+		font-size: 40px;
+		color: #d500f9;
+	}
+</style>
